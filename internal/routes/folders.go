65748d359c25333a27f3cbf8bd8d517e2a0802c0
yyko <68428103+oexlkinq@users.noreply.github.com>
2025-03-21 12:00:00 +0500

    фича: скачивание файла

--- internal/routes/folders.go
+++ internal/routes/folders.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/oexlkinq/fairdrop/internal/utils"
@@ -10,6 +11,7 @@ import (
 func (app *App) addFolderRoutes(folders *gin.RouterGroup) {
 	folders.POST("/create", app.createFolder)
 	folders.GET("/:password", app.listFolder)
+	folders.GET("/:password/:filename", app.serveFile)
 }
 
 func (app *App) createFolder(c *gin.Context) {
@@ -23,10 +25,12 @@ func (app *App) createFolder(c *gin.Context) {
 	c.JSON(http.StatusOK, folder)
 }
 
+type FolderParams struct {
+	Password string `uri:"password" binding:"required"`
+}
+
 func (app *App) listFolder(c *gin.Context) {
-	var params struct {
-		Password string `uri:"password" binding:"required"`
-	}
+	var params FolderParams
 
 	err := c.BindUri(&params)
 	if err != nil {
@@ -42,4 +46,25 @@ func (app *App) listFolder(c *gin.Context) {
 	files := app.storage.ListFolder(pw)
 
 	c.JSON(http.StatusOK, files)
+}
+
+type FileParams struct {
+	FolderParams
+	Filename string `uri:"filename" binding:"required"`
+}
+
+func (app *App) serveFile(c *gin.Context) {
+	var params FileParams
+
+	err := c.BindUri(&params)
+	if err != nil {
+		return
+	}
+
+	if strings.TrimPrefix(params.Filename, "/") == "" {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+
+	c.File(app.storage.GetFilepath(params.Password, params.Filename))
 }
