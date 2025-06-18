85231ce05d0bb390dd7afcb17ebe602a0a8b94ce
yyko <68428103+oexlkinq@users.noreply.github.com>
2025-06-19 00:34:32 +0500

    отключение кнопки загрузить когда нужно

--- frontend/src/pages/upload/index.vue
+++ frontend/src/pages/upload/index.vue
@@ -2,13 +2,56 @@
 import api from '@/api';
 import CopyBox from '@/components/CopyBox.vue';
 import HomeBtn from '@/components/HomeBtn.vue';
-import { ref, useTemplateRef } from 'vue';
+import { computed, ref, shallowRef, useTemplateRef } from 'vue';
 
 const maxFormSize = 33554432
 
 const base = import.meta.env.BASE_URL
 
 const fileInput = useTemplateRef('files')
+const filesInfo = shallowRef({
+  files: [] as File[],
+  empty: true,
+  size: 0,
+})
+
+function getFilesInfo() {
+  const res = {
+    files: [] as File[],
+    empty: true,
+    size: 0,
+  }
+
+  if (!fileInput.value) {
+    return res
+  }
+
+  if (!fileInput.value.files) {
+    throw new Error('нет files у рефа файл инпут')
+  }
+
+  const files = fileInput.value.files
+  res.files = new Array<File>(files.length)
+  for (let i = 0; i < files.length; i++) {
+    res.empty = false
+
+    const file = files.item(i)
+    if (!file) {
+      throw new Error(`null item(${i}) in files`)
+    }
+
+    res.size += file.size
+
+    res.files[i] = file
+  }
+
+  return res
+}
+
+function updateFilesInfo() {
+  filesInfo.value = getFilesInfo()
+}
+
 const uploadState = ref({
   progress: 0,
   processing: false,
@@ -22,33 +65,19 @@ async function submit() {
     return
   }
 
+  if (filesInfo.value.empty) {
+    return
+  }
+
   try {
     uploadState.value.processing = true
     uploadState.value.progress = 0
 
-    const files = fileInput.value?.files
-    if (!files) {
-      return alert('no files')
-    }
-
-    const fileArray = new Array<File>(files.length)
-    let formSize = 0
-    for (let i = 0; i < files.length; i++) {
-      const file = files.item(i)
-      if (!file) {
-        return alert(`null item(${i}) in files`)
-      }
-
-      formSize += file.size
-
-      fileArray[i] = file
-    }
-
-    if (formSize > maxFormSize) {
+    if (filesInfo.value.size > maxFormSize) {
       return alert('too big files')
     }
 
-    const { data: folder } = await api.foldersPushPost(fileArray, {
+    const { data: folder } = await api.foldersPushPost(filesInfo.value.files, {
       onUploadProgress(progressEvent) {
         uploadState.value.progress = progressEvent.progress ?? 0
       },
@@ -74,12 +103,14 @@ function makeLink(password: string) {
 
   <form class="grid-vstack" @submit.prevent="submit">
     <input type="hidden" name="MAX_FILE_SIZE" :value="maxFormSize">
-    <input class="form-control" type="file" name="file" ref="files" multiple :disabled="uploadState.processing">
+    <input class="form-control" type="file" name="file" ref="files" multiple :disabled="uploadState.processing"
+      @change.passive="updateFilesInfo">
 
-    <input class="btn btn-primary w-100" type="submit" value="Загрузить" :disabled="uploadState.processing">
+    <input class="btn btn-primary w-100" type="submit" value="Загрузить"
+      :disabled="uploadState.processing || uploadState.success || filesInfo.empty">
 
     <div class="progress" :aria-disabled="!uploadState.processing">
-      <div class="progress-bar" :style="{ width: `${uploadState.progress * 100}%` }"></div>
+      <div class="progress-bar progress-bar-striped" :style="{ width: `${uploadState.progress * 100}%` }"></div>
     </div>
 
     <div v-show="uploadState.success">
