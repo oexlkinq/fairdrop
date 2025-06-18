41a4fbb30ef3a1d517d11e6d93c0d3879545e466
yyko <68428103+oexlkinq@users.noreply.github.com>
2025-06-19 00:32:00 +0500

    фикс выбора basePath

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import { Configuration, DefaultApi } from "./client";
 
-const publicBasePath = import.meta.env.VITE_PUBLIC_BASE_PATH as string | undefined
+const publicBasePath = import.meta.env.VITE_PUBLIC_BASE_PATH || import.meta.env.BASE_URL
 
 const conf = new Configuration({
   basePath: publicBasePath
